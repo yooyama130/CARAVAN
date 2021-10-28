d58c257a5f55dc0ba6ec6d0202f60389cc28a88a
EC2 Default User <ec2-user@ip-172-31-17-245.ec2.internal>
2021-10-28 06:29:57 +0000

    [Finish] 3-2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.hosts << "5b71068fad9441f083438a30e1d9c9a5.vfs.cloud9.us-east-1.amazonaws.com"
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -28,6 +29,8 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
+
+
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
